6cdc313470dac4f6eb2d171a061f388924fe0b26
David Alba-Lopez <albalopezdavid@gmail.com>
2020-03-10 08:54:45 -0400

    Test Commit 1

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by CSSE Department on 3/10/20.
+//  Copyright © 2020 Rose-Hulman CSSE484. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello world!")
+    }
+
+
+}
+
